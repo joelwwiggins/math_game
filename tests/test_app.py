3ddb732facd1b45d776187b9e24682b9efc56259
Joel Wiggins <joelwwiggins@gmail.com>
2025-04-05 20:06:29 -0500

    got it running locally. minimized workflow. changed to init_db. moving to get container apps running

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 import sqlite3
 from app import app, get_db, init_db, close_db, create_player
 
@@ -7,12 +8,24 @@ class MathGameTestCase(unittest.TestCase):
         """Set up test environment."""
         self.app = app.test_client()
         self.app.testing = True
+        
+        # For testing only, temporarily modify DB host if it's not available
+        # This allows tests to run even when the DB container isn't present
+        self.original_db_host = os.environ.get('POSTGRES_HOST')
+        if self.original_db_host == 'mathgame-db':
+            # During tests, set to localhost or mock
+            os.environ['POSTGRES_HOST'] = 'db'  
+
         with app.app_context():
             init_db()
             self.player_id = create_player('Test Player')
 
     def tearDown(self):
         """Tear down test environment."""
+        # Restore original DB host if we changed it
+        if self.original_db_host:
+            os.environ['POSTGRES_HOST'] = self.original_db_host
+
         with app.app_context():
             db = get_db()
             with db.cursor() as cursor:
